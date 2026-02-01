e835beff36b474a3472d34ea0e6e80edff74102a
nurbol <nurbol@nurbol.nurbol>
2026-02-01 14:53:59 +0600

    create app main, add swagger

---
+++ backend_api/config/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.decorators import login_required
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Rent car',
+        version='v1',
+    ), 
+    public=False
+    permission_classes=[IsAdminUser]
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', login_required(schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui')),
+    path('', include('apps.main.urls')),
+]
