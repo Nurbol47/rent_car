19aa6bca71e0f4506d7672bb0709a4a2b5eaa3ae
nurbol <nurbol@nurbol.nurbol>
2026-02-14 17:12:46 +0600

    fixed inlines

--- backend_api/apps/location/inlines.py
+++ backend_api/apps/location/inlines.py
@@ -3,9 +3,9 @@ from .models import *
 
 class CharacteristicInline(admin.TabularInline):
     model = Characteristic
-    extra = 1
+    extra = 2
 
 
 class ImgLocationInline(admin.TabularInline):
     model = ImgLocation
-    extra = 1
+    extra = 3
